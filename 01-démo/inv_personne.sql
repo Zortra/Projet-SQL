e43ab785ccce8deefef3ce9a07ed519f9e9da6f7
Zortra <mathilde.chantaraud@gmail.com>
2024-04-02 14:50:47 +0200

    TP

---
+++ 01-démo/inv_personne.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 02 avr. 2024 à 11:37
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `invitation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inv_personne`
+--
+
+DROP TABLE IF EXISTS `inv_personne`;
+CREATE TABLE IF NOT EXISTS `inv_personne` (
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` int NOT NULL,
+  `date_inscription` date NOT NULL,
+  `status_valide` tinyint(1) NOT NULL,
+  `type_membre` enum('Valide','NON Valide') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description_` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `salaire` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
